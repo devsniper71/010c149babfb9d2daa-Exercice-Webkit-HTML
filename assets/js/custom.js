5650e17d911aac1254dcb5be9d28c51f3fff26f3
Md Rasel Ahmed <raselism71@gmail.com>
2023-06-09 19:09:10 +0600

    Update custom.js

--- assets/js/custom.js
+++ assets/js/custom.js
@@ -7,41 +7,41 @@ const menuIcon = document.getElementById("menuIcon");
 const closeIcon = document.getElementById("closeIcon");
 
 mobileMenuButton.addEventListener("click", () => {
-    mobileMenu.classList.toggle("hidden");
-    menuIcon.classList.toggle("hidden");
-    closeIcon.classList.toggle("hidden");
-    const expanded = mobileMenuButton.getAttribute("aria-expanded") === "true";
-    mobileMenuButton.setAttribute("aria-expanded", !expanded);
+  mobileMenu.classList.toggle("hidden");
+  menuIcon.classList.toggle("hidden");
+  closeIcon.classList.toggle("hidden");
+  const expanded = mobileMenuButton.getAttribute("aria-expanded") === "true";
+  mobileMenuButton.setAttribute("aria-expanded", !expanded);
 });
 
 /*-----------------------------
 ---Accordion menu Function-----
 -----------------------------*/
 window.addEventListener("DOMContentLoaded", function () {
-    var accordionItems = document.getElementsByClassName("accordion-item");
+  var accordionItems = document.getElementsByClassName("accordion-item");
 
-    // Open the first item by default
-    accordionItems[0].classList.add("active");
-    accordionItems[0].querySelector(".accordion-content").style.display = "block";
+  // Open the first item by default
+  accordionItems[0].classList.add("active");
+  accordionItems[0].querySelector(".accordion-content").style.display = "block";
 
-    // Add click event listener to each accordion item
-    Array.prototype.forEach.call(accordionItems, function (item) {
-        var header = item.querySelector(".accordion-header");
-        var content = item.querySelector(".accordion-content");
+  // Add click event listener to each accordion item
+  Array.prototype.forEach.call(accordionItems, function (item) {
+    var header = item.querySelector(".accordion-header");
+    var content = item.querySelector(".accordion-content");
 
-        header.addEventListener("click", function () {
-            // Toggle active class and display content
-            var isActive = item.classList.contains("active");
+    header.addEventListener("click", function () {
+      // Toggle active class and display content
+      var isActive = item.classList.contains("active");
 
-            Array.prototype.forEach.call(accordionItems, function (item) {
-                item.classList.remove("active");
-                item.querySelector(".accordion-content").style.display = "none";
-            });
+      Array.prototype.forEach.call(accordionItems, function (item) {
+        item.classList.remove("active");
+        item.querySelector(".accordion-content").style.display = "none";
+      });
 
-            if (!isActive) {
-                item.classList.add("active");
-                content.style.display = "block";
-            }
-        });
+      if (!isActive) {
+        item.classList.add("active");
+        content.style.display = "block";
+      }
     });
+  });
 });
